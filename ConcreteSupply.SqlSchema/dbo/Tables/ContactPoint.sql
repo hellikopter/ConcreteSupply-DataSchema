79af75a02c88cd67837ba8754884d76e7b6931ce
Wojciech Stawiarski <hellikopter@users.noreply.github.com>
2020-04-24 16:36:02 +0100

    correct names

--- ConcreteSupply.SqlSchema/dbo/Tables/ContactPoint.sql
+++ ConcreteSupply.SqlSchema/dbo/Tables/ContactPoint.sql
@@ -1,13 +1,15 @@
 ﻿CREATE TABLE [dbo].[ContactPoint] (
     [Id]                INT           IDENTITY (1, 1) NOT NULL,
-    [Label]             NVARCHAR (64) NOT NULL,
+    [ContactPointLabel] NVARCHAR (64) NOT NULL,
     [Email]             NVARCHAR (64) NULL,
     [PhoneNumber]       NVARCHAR (16) NULL,
     [MobilePhoneNumber] NVARCHAR (16) NULL,
     [RdfLocalId]        NVARCHAR (36) NOT NULL,
     CONSTRAINT [PK_ContactPoint] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [IX_ContactPoint] UNIQUE NONCLUSTERED ([Label] ASC)
+    CONSTRAINT [IX_ContactPoint] UNIQUE NONCLUSTERED ([ContactPointLabel] ASC)
 );
+
+
 
 
 
--- ConcreteSupply.SqlSchema/dbo/Tables/Customer.sql
+++ ConcreteSupply.SqlSchema/dbo/Tables/Customer.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[Customer] (
-    [Id]         INT           IDENTITY (1, 1) NOT NULL,
-    [Label]      NVARCHAR (64) NOT NULL,
-    [RdfLocalId] NVARCHAR (36) NOT NULL,
+    [Id]            INT           IDENTITY (1, 1) NOT NULL,
+    [CustomerLabel] NVARCHAR (64) NOT NULL,
+    [RdfLocalId]    NVARCHAR (36) NOT NULL,
     CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [IX_Customer] UNIQUE NONCLUSTERED ([Label] ASC)
+    CONSTRAINT [IX_Customer] UNIQUE NONCLUSTERED ([CustomerLabel] ASC)
 );
+
+
 
 
 
